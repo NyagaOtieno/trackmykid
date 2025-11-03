29e72b4222927c21c95992b666ecf3e915840d51
Kento <nyagaotieno@gmail.com>
2025-11-03 09:21:46 +0300

    Fix SPA refresh with Express server

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,23 @@
 import { defineConfig, ViteDevServer } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 import history from "connect-history-api-fallback"; // ✅ Import this
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig(() => ({
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === "development" && componentTagger(),
     {
       name: "html-fallback",
       configureServer(server: ViteDevServer) {
         server.middlewares.use(history());
       },
     },
-  ].filter(Boolean),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
