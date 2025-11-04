3b7d233a2ca037aa421e4c627bd47d5863eaef06
Kento <nyagaotieno@gmail.com>
2025-11-04 22:56:17 +0300

    Initial commit – fresh install and build setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,20 @@
-import { defineConfig, ViteDevServer } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import history from "connect-history-api-fallback"; // ✅ Import this
 
-// https://vitejs.dev/config/
-export default defineConfig(() => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    {
-      name: "html-fallback",
-      configureServer(server: ViteDevServer) {
-        server.middlewares.use(history());
-      },
-    },
-  ],
+export default defineConfig({
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+  },
+  server: {
+    host: "::",
+    port: 8080,
+  },
+});
