703044f26147de519f5a62d2dc2b1f56b418fe97
Kento <nyagaotieno@gmail.com>
2025-11-04 23:29:52 +0300

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -7,10 +7,10 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "src"),
     },
   },
   build: {
-    outDir: "dist", // Vercel expects this directory
-  },
+    outDir: "dist"
+  }
 });
